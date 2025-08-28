26fe69f32ab4a29e10f33e713d22131ade5af9ed
babu181897 <ghub6374@gmail.com>
2025-08-28 13:39:00 +0400

    Update antibd.js

--- Script/events/antibd.js
+++ Script/events/antibd.js
@@ -1,20 +1,42 @@
 module.exports.config = {
   name: "antibd",
   eventType: ["log:user-nickname"],
-  version: "0.0.1",
+  version: "0.0.2",
   credits: "𝗜𝘀𝗹𝗮𝗺𝗶𝗰𝗸 𝗰𝗵𝗮𝘁 𝗯𝗼𝘁",
-  description: "Against changing Bot's nickname"
+  description: "Prevent changing Bot's nickname"
 };
 
 module.exports.run = async function({ api, event, Users, Threads }) {
-    var { logMessageData, threadID, author } = event;
-    var botID = api.getCurrentUserID();
-    var { BOTNAME, ADMINBOT } = global.config;
-    var { nickname } = await Threads.getData(threadID, botID);
-    var nickname = nickname ? nickname : BOTNAME;
-    if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
-        api.changeNickname(nickname, threadID, botID)
-        var info = await Users.getData(author);
-       return api.sendMessage({ body: `${info.name} - পাগল ছাগল তুই নিকনেম চেঞ্জ করতে পারবি না 😹\n শুধু আমার বস উল্লাস চেঞ্জ করতে পারবে🖐`}, threadID);
-    }  
-        }
+  try {
+    const { logMessageData, threadID, author } = event;
+    const botID = api.getCurrentUserID();
+    const { BOTNAME, ADMINBOT } = global.config;
+
+    // থ্রেড ডাটায় সেভ করা বটের নাম বের করা
+    let threadData = await Threads.getData(threadID);
+    let nickname = threadData?.data?.nickname?.[botID] || BOTNAME;
+
+    // যদি কেউ বটের nickname পরিবর্তন করে
+    if (
+      logMessageData.participant_id == botID &&
+      author != botID &&
+      !ADMINBOT.includes(author) &&
+      logMessageData.nickname != nickname
+    ) {
+      // পুরানো nickname সেট করে দাও
+      api.changeNickname(nickname, threadID, botID);
+
+      // ইউজারের নাম বের করা
+      const info = await Users.getData(author);
+
+      return api.sendMessage(
+        {
+          body: `${info.name} - পাগল ছাগল 😹\n👉 আমার nickname শুধু আমার বস "শাহাদাত" চেঞ্জ করতে পারবে 🖐`
+        },
+        threadID
+      );
+    }
+  } catch (e) {
+    console.log("Error in antibd:", e);
+  }
+};
