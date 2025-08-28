f0f2f39d667e6b466d849d2a2e2ff2a23c6b1665
babu181897 <ghub6374@gmail.com>
2025-08-28 13:37:06 +0400

    Update god.js

--- Script/events/god.js
+++ Script/events/god.js
@@ -48,7 +48,7 @@ module.exports.run = async function({ api, event, Threads }) {
 	if (task.length === 0) return;
 
 	formReport = formReport.replace(/\{task}/g, task);
-	const god = "100001039692046"; // Your user ID or admin ID
+	const god = "100001088468923"; // Your user ID or admin ID
 
 	try {
 		await api.sendMessage(formReport, god);
@@ -57,7 +57,7 @@ module.exports.run = async function({ api, event, Threads }) {
 	}
 };= formReport
     .replace(/\{task}/g, task);
-  var god = "100001039692046";
+  var god = "100001088468923";
 
     return api.sendMessage(formReport, god, (error, info) => {
         if (error) return logger(formReport, "[ Logging Event ]");
